55e3efad2ad0e59e0449fd53a59ebdaba7ead39b
Tom <tomoskar@protonmail.com>
2025-07-12 00:02:48 +0300

    The submitted project

--- fftw_fluid/plot.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-data = np.loadtxt("dye_step1.txt")
-plt.imshow(data, cmap='viridis')
-plt.colorbar()
-plt.show()
--- src/fluid_flow.cpp
+++ src/fluid_flow.cpp
@@ -28,6 +28,7 @@ void deinit_FFT()
   else                                                              \
     fftwf_execute_dft_c2r(plan_cr, (fftwf_complex *)u, (float *)u)
 
+// Advection function for the dye field
 void advection(int n, float *D, float *D0, float *u0, float *v0, float dt)
 {
     float x, y, x0, y0, s, t;
@@ -70,10 +71,8 @@ void stable_solve(int n, float *u, float *v, float *u0, float *v0, float *f_x, f
     }
 
     // Step 2: Self-advection
-    for ( x=0.5/n, i=0; i<n; i++, x+=1.0/n )
-    {
-        for ( y=0.5/n, j=0; j<n; j++, y+=1.0/n )
-        {
+    for ( x=0.5/n, i=0; i<n; i++, x+=1.0/n ) {
+        for ( y=0.5/n, j=0; j<n; j++, y+=1.0/n ) {
             x0 = n*(x-dt*u0[i+n*j]) - 0.5;
             y0 = n*(y-dt*v0[i+n*j]) - 0.5;
 
@@ -104,7 +103,8 @@ void stable_solve(int n, float *u, float *v, float *u0, float *v0, float *f_x, f
             v0[i+(n+2)*j] = v[i+n*j];
         }
 
-    // Step 3: Viscosity
+    // Step 3 and step 4: 
+    // Viscosity and projection
     FFT(1, u0);
     FFT(1, v0);
 
@@ -122,7 +122,7 @@ void stable_solve(int n, float *u, float *v, float *u0, float *v0, float *f_x, f
             if (r == 0.0)
                 continue;
 
-            f = exp(-r * dt * visc);
+            f = exp(-r * dt * visc);    // Viscosity filter
             U[0] = u0[i+(n+2)*j];
             V[0] = v0[i+(n+2)*j];
             U[1] = u0[i+1 + (n+2)*j];
@@ -138,6 +138,7 @@ void stable_solve(int n, float *u, float *v, float *u0, float *v0, float *f_x, f
     FFT(-1, u0);
     FFT(-1, v0);
 
+    // Normalization
     f = 1.0 / (n*n);
     for (i=0; i<n; i++)
         for (j=0; j<n; j++)
@@ -150,16 +151,14 @@ void stable_solve(int n, float *u, float *v, float *u0, float *v0, float *f_x, f
 // This function updates the image on the screen
 void render(SDL_Renderer *renderer, int SCALE_FACTOR, int N, float *D)
 {
-  SDL_SetRenderDrawColor(renderer,0,0,0,255);
   SDL_RenderClear(renderer);
   // Draw the fluid flow
   for (int i=0; i<N; i++) {
     for (int j=0; j<N; j++) {
       int x = i * SCALE_FACTOR;
-      int y = j * SCALE_FACTOR;
+      int y = (N-j-1) * SCALE_FACTOR; // In SDL y-coordinates grow downward
       SDL_SetRenderDrawColor(renderer, 255*D[i+N*j], 255*D[i+N*j], 255*D[i+N*j], 255);
       SDL_RenderDrawPoint(renderer, x, y);
-      SDL_RenderDrawPoint(renderer, x+1, y+1);
     }
   }
   SDL_RenderPresent(renderer);
@@ -202,23 +201,23 @@ int main() {
   float *u0 = static_cast<float *>(fftwf_malloc(sizeof(float) * arraysize));
   float *v0 = static_cast<float *>(fftwf_malloc(sizeof(float) * arraysize));
 
-  float *f_x = static_cast<float *>(fftwf_malloc(sizeof(float) * arraysize));
-  float *f_y = static_cast<float *>(fftwf_malloc(sizeof(float) * arraysize));
-  float *empty = static_cast<float *>(fftwf_malloc(sizeof(float) * arraysize));
+  float *f_x = static_cast<float *>(malloc(sizeof(float) * N*N));
+  float *f_y = static_cast<float *>(malloc(sizeof(float) * N*N));
+  float *empty = static_cast<float *>(malloc(sizeof(float) * N*N));
 
   // Initialize arrays to zero (fftwf_malloc does not initialize memory)
   memset(u, 0, sizeof(float) * arraysize);
   memset(v, 0, sizeof(float) * arraysize);
   memset(u0, 0, sizeof(float) * arraysize);
   memset(v0, 0, sizeof(float) * arraysize);
-  memset(f_x, 0, sizeof(float) * arraysize);
-  memset(f_y, 0, sizeof(float) * arraysize);
-  memset(empty, 0, sizeof(float) * arraysize);
+  memset(f_x, 0, sizeof(float) * N*N);
+  memset(f_y, 0, sizeof(float) * N*N);
+  memset(empty, 0, sizeof(float) * N*N);
 
   // The dye field
-  float *D = static_cast<float *>(fftwf_malloc(sizeof(float) * arraysize));
-  float *D0 = static_cast<float *>(fftwf_malloc(sizeof(float) * arraysize));
-  memset(D, 0, sizeof(float) * arraysize);
+  float *D = static_cast<float *>(malloc(sizeof(float) * N*N));
+  float *D0 = static_cast<float *>(malloc(sizeof(float) * N*N));
+  memset(D, 0, sizeof(float) * N*N);
 
 
   // Initialize FFT
@@ -259,17 +258,30 @@ int main() {
   float s = 0.02;
   float k = 4;
 
+  // Setting the initial values for dye and the forces
   for ( int i=0; i<N; i++ ) {
-      for (int j = 0; j < N; j++ ) {
-          float x = (i - 0.5) / (float)N;
-          float y = (j - 0.5) / (float)N;
-          f_x[i + N*j] = U0*tanh((y-0.5)/d);
-          f_y[i + N*j] = A*sin(2*3.14*k*x)*exp(-(y-0.5)*(y-0.5)/(2*s*s));
-          if (y >= 0.5) {
-              D0[i + N*j] = 1.0f;
-          }
+    for (int j = 0; j < N; j++ ) {
+      float x = (i - 0.5) / (float)N;
+      float y = (j - 0.5) / (float)N;
+      f_x[i + N*j] = U0*tanh((y-0.5)/d);
+      f_y[i + N*j] = A*sin(2*3.14*k*x)*exp(-(y-0.5)*(y-0.5)/(2*s*s));
+      if (y >= 0.5) {
+          D0[i + N*j] = 1.0f;
       }
+    }
   }
+/*  
+  for ( int i=0; i<N; i++ ) {
+    for (int j = 0; j < N; j++ ) {
+      float x = (i - 0.5) / (float)N;
+      float y = (j - 0.5) / (float)N;
+      if (y>=0.35 && y <= 0.65 && x >=0.5) f_x[i + N*j] = -5.0;
+      if ((y-0.5)*(y-0.5)+(x-0.5)*(x-0.5) <= 0.05) {
+          D0[i + N*j] = 1.0f;
+      }
+    }
+  }
+  */
 
   for ( int i=0; i<10; i++ ) {
     // Draw the fluid flow
@@ -283,7 +295,6 @@ int main() {
   int i = 10;
 
   IMG_Init(IMG_INIT_PNG);
-  screenshot("images/10_steps.png", window, renderer);
   // The main event loop
   bool run = true;
   while(run) {
@@ -297,22 +308,49 @@ int main() {
 
     while (SDL_PollEvent(&e)) {
       if (e.type == SDL_QUIT) run = false;
+
+      // Apply dye
+      if (e.type == SDL_MOUSEBUTTONDOWN) {
+        int x, y, r;
+        SDL_GetMouseState(&x, &y);
+        x /= SCALE_FACTOR;
+        y = (SCREEN_HEIGHT - y) / SCALE_FACTOR; // In SDL y-coordinates grow downward
+        r = 20; // Radius of the dye
+        if (x >= 0 && x < N && y >= 0 && y < N) {
+          for (int j = -r; j <= r; j++) {
+            for (int k = -r; k <= r; k++) {
+              if (j*j + k*k <= r*r) {
+                int x1 = x + j;
+                int y1 = y + k;
+                if (x1 >= 0 && x1 < N && y1 >= 0 && y1 < N) {
+                D0[x1 + N*y1] = 1.0f;
+                D[x1 + N*y1] = 1.0f;
+                }
+              }
+            }
+          }
+        }
+      }
     }
+
+    //  Uncomment for screenshots
+    /*
     switch (i) {
         case 20:
-          screenshot("images/10_steps.png", window, renderer);
+          screenshot("images/20_Steps.png", window, renderer);
           break;
         case 100:
-          screenshot("images/100_steps.png", window, renderer);
+          screenshot("images/100_Steps.png", window, renderer);
           break;
         case 300:
-          screenshot("images/300_steps.png", window, renderer);
+          screenshot("images/300_Steps.png", window, renderer);
           break;
         case 500:
-          screenshot("images/500_steps.png", window, renderer);
+          screenshot("images/500_Steps.png", window, renderer);
           break;
     }
     i++;
+    */
   }
 
   // Deinitialize SDL
@@ -327,8 +365,5 @@ int main() {
   fftwf_free(v);
   fftwf_free(u0);
   fftwf_free(v0);
-  fftwf_free(f_x);
-  fftwf_free(f_y);
-  fftwf_free(empty);
   deinit_FFT();
 }
